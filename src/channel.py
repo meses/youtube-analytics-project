e5c4bb6cf6634f712fda8872c35f0da400fe4386
Artem <akirillov@itps-russia.ru>
2023-03-10 02:02:10 +0500

    Реализован класс и вывод инфо о канале

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,33 @@
+import json
+import os
 
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
+
+import isodate
+
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+#Компьютер корпоративный. Из-за особенностей политик безопасности я не могу установить ключ в переменную среды своего пользователя
+#Поэтому ключ вставляю в код просто строкой
+api_key: str = '********'
+
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        printj(channel)
